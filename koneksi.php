0f88b9af867fb0492b7e51ff2052d785b2ef7d78
Dakara <dakaraputra@gmail.com>
2026-02-03 14:33:21 +0700

    konek database setelah deploy

--- koneksi.php
+++ koneksi.php
@@ -1,13 +1,8 @@
 <?php
-$host     = 'localhost';
-$port     = '5433'; 
-$user     = 'postgres';
-$password = 'dakput01';
-$db       = 'todonote'; 
+$connStr = getenv("DATABASE_URL");
+$db = pg_connect($connStr);
 
-$conn = pg_connect("host=$host port=$port dbname=$db user=$user password=$password");
-
-if (!$conn) {
-    die("Koneksi Gagal: " . pg_last_error());
+if (!$db) {
+    die("Koneksi gagal: " . pg_last_error());
 }
 ?>
